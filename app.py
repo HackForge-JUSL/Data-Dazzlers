9650975ddd2005be4f25e9cf76f14ac134de846a
Sufyan <bucks.pri@gmail.com>
2024-03-16 17:05:31 +0530

    making the app intial commit

--- app.py
+++ app.py
@@ -6,3 +6,12 @@ from flask import Flask,render_template, request,redirect,Response
 app = Flask(__name__)
 
 cameras={}
+
+
+@app.route('/')
+def dashboard():
+    return render_template('dashboard.html',cameras=cameras)
+
+
+if __name__=="__main__":
+    app.run()
