50d8efd6d8089a15bcc71d06e213acf6678618f3
Sufyan <bucks.pri@gmail.com>
2024-03-16 11:13:26 +0530

    addded function to people.py

--- models/people.py
+++ models/people.py
@@ -15,6 +15,39 @@ class people_model():
     """
 
     def __init__(self,model_path,region,conf=0.85):
+        """
+        basic inti function
+        """
         self.model=YOLO(model_path,verbose=False)
         self.region=region
         self.conf=conf
+
+    def in_region(self, point):
+        """
+        this function checks is the given point is in region
+        """
+        x, y = point
+        x1, y1, x2, y2 = self.region
+        return x1 <= x <= x2 and y1 <= y <= y2
+    
+    def process(self,frame):
+        """
+        this function processes the cv2 frame and returns the
+        bounding boxes
+        """
+
+        bb_boxes=[]
+        results=self.model(img,verbose=False)
+
+        for box in results[0].boxes:
+            if (int(box.cls[0])==0 and float(box.conf[0])>0.75):
+                bb=list(map(int,box.xyxy[0]))
+                center=[(bb[0]+bb[2])//2,(bb[1]+bb[3])//2]
+
+                if(self.in_region(center)):
+                    bb_boxes.append((True,bb))
+
+        if not len(bb_boxes):
+            bb_boxes.append((False,[]))
+        
+        return bb_boxes
