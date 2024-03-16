b78bc0b79904e7ce77ec6b9c4f5f93f8e25280ad
Sufyan <bucks.pri@gmail.com>
2024-03-16 11:15:17 +0530

    modifiaction : people.py line 40

--- models/people.py
+++ models/people.py
@@ -30,7 +30,7 @@ class people_model():
         x1, y1, x2, y2 = self.region
         return x1 <= x <= x2 and y1 <= y <= y2
     
-    def process(self,frame):
+    def process(self,img):
         """
         this function processes the cv2 frame and returns the
         bounding boxes
