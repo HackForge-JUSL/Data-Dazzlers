a86ac6aee105cfc8d3b8ae6c8a46d6864502c64e
Navya <navyac.social@gmail.com>
2024-03-16 13:56:38 +0530

    smoking.py updated

---
+++ models/smoking.py
@@ -0,0 +1,24 @@
+from ultralytics import YOLO
+import cv2
+import cvzone
+
+class smoking_detection():
+    def __init__(self,model_path,conf=0.85):
+        self.model = YOLO(model_path,verbose=False)
+        self.confidence = conf
+
+    def process(self,img):
+        bb_boxes=[]
+        result = self.model(img,verbose=False)
+
+        for box in result[0].boxes:
+            if(float(box.conf[0])>self.confidence):
+                bb=list(map(int,box.xyxy[0]))
+                bb_boxes.append(bb)
+
+        
+        if(len(bb_boxes)):
+            found=True
+        else:
+            found=False
+        return(found,bb_boxes)
